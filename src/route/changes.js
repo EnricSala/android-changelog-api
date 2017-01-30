6fe218a4df8ca7ccfea91b995fab770f0e1acf3a
Enric Sala <sinh32.enric@gmail.com>
2017-01-30 09:57:58 +0100

    Remove no longer used constants

--- src/route/changes.js
+++ src/route/changes.js
@@ -2,9 +2,6 @@ const router = require('express').Router();
 
 const scraper = require('../service/scraper');
 
-const baseUrl = 'https://play.google.com/store/apps/details?hl=en&id=';
-const headers = { 'Accept-Language': 'en-US,en;q=0.8' };
-
 router.get('/', (req, res, next) => {
     const packageName = req.query.package;
 
