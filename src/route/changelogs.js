cc4f209f011639d4f6888a39e71b1145e58cf5c5
Enric Sala <sinh32.enric@gmail.com>
2017-02-02 11:47:40 +0100

    Change package query from query param to path variable

--- src/route/changelogs.js
+++ src/route/changelogs.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 const scraper = require('../service/scraper');
 
-router.get('/', (req, res, next) => {
-    const packageName = req.query.package;
+router.get('/:package/latest', (req, res, next) => {
+    const packageName = req.params.package;
 
     if (!packageName) {
         const err = new Error('Undefined query');
